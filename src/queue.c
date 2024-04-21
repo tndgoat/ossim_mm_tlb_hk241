98a7b23db20528a194ca42735ae5f1d423e4e52a
Nguyen Khoa <khoa.nguyenmbk22351@hcmut.edu.vn>
2024-04-21 23:48:36 +0700

    queue + sched

--- src/queue.c
+++ src/queue.c
@@ -9,12 +9,27 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+        if (q->size < MAX_QUEUE_SIZE) {
+                q->proc[q->size++] = proc;
+        }
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+        if (q->size == 0){
+                return NULL;
+        }
+
+        struct pcb_t* tmp = q->proc[0];
+        q->size--;
+
+        for (int i = 0; i < q->size; i++){
+                q->proc[i] = q->proc[i + 1];
+        }
+        q->proc[q->size] = NULL;
+        
+	return tmp;
 }
 
--- src/sched.c
+++ src/sched.c
@@ -27,8 +27,10 @@ void init_scheduler(void) {
 #ifdef MLQ_SCHED
 	int i ;
 
-	for (i = 0; i < MAX_PRIO; i ++)
+	for (i = 0; i < MAX_PRIO; i ++){
 		mlq_ready_queue[i].size = 0;
+		mlq_ready_queue[i].slot = MAX_PRIO - i;
+	}
 #endif
 	ready_queue.size = 0;
 	run_queue.size = 0;
@@ -47,8 +49,37 @@ struct pcb_t * get_mlq_proc(void) {
 	/*TODO: get a process from PRIORITY [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 */
-	proc = dequeue(&mlq_ready_queue[0]);
-	return proc;	
+	if (queue_empty()){
+		return proc;
+	}
+
+	int check_empty = 0;
+	pthread_mutex_lock(&queue_lock);
+
+	for(int i = 0; i <= MAX_PRIO; i++){	
+		if (i != MAX_PRIO) {
+			if(!empty(&mlq_ready_queue[i])){
+				if(mlq_ready_queue[i].slot == 0){
+					check_empty = 1;
+					continue;
+				}
+				mlq_ready_queue[i].slot--;	
+				proc = dequeue(&mlq_ready_queue[i]);
+				break;
+			}
+		}
+		else {
+			for (int j = 0; j < MAX_PRIO; j++) {
+				mlq_ready_queue[j].slot = MAX_PRIO - j;
+			}
+			if (check_empty){
+				i = -1;
+			}
+		}
+	}
+	
+	pthread_mutex_unlock(&queue_lock);
+	return proc;
 }
 
 void put_mlq_proc(struct pcb_t * proc) {
